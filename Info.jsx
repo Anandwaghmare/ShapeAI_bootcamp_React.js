811253b9f0f526bd198f1d6b1dbff7df1aea8683
Anandwaghmare <86519874+Anandwaghmare@users.noreply.github.com>
2021-06-27 01:41:12 -0700

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>about us</h1>
+      <p>we provide a books with good discount since last 10 year</p>
+    </div>
+  );
+}
+export default Info;
